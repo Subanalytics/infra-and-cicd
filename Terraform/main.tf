bd109e76be97ab7db2cc60e89c814939d73daa07
Tushar <96531426+tush1809@users.noreply.github.com>
2025-09-11 18:07:15 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,8 +7,17 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "tushar-terraform-state"     # must be globally unique
+    key            = "env/tushar/terraform.tfstate"  
+    region         = "us-east-1"                         
+    dynamodb_table = "tushar-terraform-locks"     
+    encrypt        = true                                
+  }
 }
+
 
 provider "aws" {
   region = var.aws_region
 }
+
